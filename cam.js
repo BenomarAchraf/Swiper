e0a99df86448abc5b4d20f6d2674a56b57d922d9
BenomarAchraf <101630642+BenomarAchraf@users.noreply.github.com>
2024-09-09 22:48:52 +0100

    photobooth-v2

---
+++ cam.js
@@ -0,0 +1,50 @@
+const data = [
+    {   
+        personnage: "blazer",
+        prompt:"portrait of a man wearing blazer with deep navy with a subtle sheen paired with a crisp white shirt with high-end restaurant with elegant decor and soft, ambient lighting background",
+        src: "https://d3aa3s3yhl0emm.cloudfront.net/aiavatar/0954fc6a-835d-40f4-8b26-f3e888254301.jpg",
+    },
+    {   
+        personnage: "Officer",
+        prompt:"portrait of a man with brown hair, a police officer uniform in khaki uniform, close-up, front face",
+        src: "https://d3aa3s3yhl0emm.cloudfront.net/aiavatar/5398aae1-7831-46e2-897b-03b87f453b71.jpg",
+    },
+    {   
+        personnage: "Cop",
+        prompt:"portrait of a man with brown hair police officer uniform in dark black and white uniform , close-up ,front face",
+        src: "https://d3aa3s3yhl0emm.cloudfront.net/aiavatar/b6de3ac1-203f-418d-9313-a59fad5563df.jpg",
+    },
+    {   
+        personnage: "Barbarian",
+        prompt:"portrait of a male barbarian warrior, wearing leather armor, furs, rugged weapons, and a wild and untamed appearance,extremely detailed skin,extremely detailed clothes,radiant light,cinematic,establishing shot, warland in background",
+        src: "https://d3aa3s3yhl0emm.cloudfront.net/aiavatar/40f33ae8-c91a-4a14-940a-3d875e9b7542.jpg",
+    },
+    {   
+        personnage: "KuwaitMan",
+        prompt:"Portrait of a Kuwaiti man wearing traditional Kuwaiti attire, including a white dishdasha and ghutra, in a high-end restaurant with elegant decor and soft, ambient lighting in the background.",
+        src: "https://d3aa3s3yhl0emm.cloudfront.net/aiavatar/f16530f0-d184-454c-a94d-ff97e7d90927.jpg",
+    },
+    {   
+        personnage: "KuwaitWoman",
+        prompt:"Portrait of a woman wearing traditional Kuwaiti attire, including a black abaya and hijab, in a high-end restaurant with elegant decor and soft, ambient lighting in the background.",
+        src: "https://d3aa3s3yhl0emm.cloudfront.net/aiavatar/360b9861-566a-42f9-bb9a-d15de6975e3b.jpg",
+    }
+    ,
+    {   
+        personnage: "KuwaitLady",
+        prompt:"Portrait of a woman wearing traditional Kuwaiti attire, including a black abaya and hijab, in a high-end restaurant with elegant decor and soft, ambient lighting in the background.",
+        src: "https://d3aa3s3yhl0emm.cloudfront.net/aiavatar/0e36bb2a-b177-494f-bfac-796774a3a32b.jpg",
+    }
+    ,
+    {   
+        personnage: "sciencefiction",
+        prompt:"portrait of a magical Wisdom god, energy bolts, sparkles, lights, blue skinned man, huge glowing yellow eyes, short dark hair, flowing hair, 8k resolution concept art portrait",
+        src: "https://d3aa3s3yhl0emm.cloudfront.net/aiavatar/c085e77c-9059-4af2-a2dd-978af8c69c21.jpg",
+    } ,
+    {   
+        personnage: "sciencefiction",
+        prompt:"portrait of a magical Wisdom god, energy bolts, sparkles, lights, blue skinned man, huge glowing yellow eyes, short dark hair, flowing hair, 8k resolution concept art portrait",
+        src: "https://d3aa3s3yhl0emm.cloudfront.net/aiavatar/c085e77c-9059-4af2-a2dd-978af8c69c21.jpg",
+    }
+
+]
